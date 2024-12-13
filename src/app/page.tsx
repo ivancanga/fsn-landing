d05f4cea60cdebf860810cde9b8e2383a779ae6e
Iván Canga <ivancanga@outlook.com>
2024-12-12 22:02:25 -0300

    Landing v1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Countdown from "./components/countdown";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen bg-black text-white">
+    <div className="flex flex-col items-center h-screen bg-black text-white overflow-hidden">
       <Logo>
         <h1>FSN 7</h1>
       </Logo>
@@ -17,12 +17,12 @@ export default function Home() {
         <Laser />
         <Laser2 />
         <img src="/diamond.gif" className="w-32 h-32" />
-
       </DiamondContainer>
 
       <DiagonalInverted />
       <div className="bg-white text-black w-full text-center">
         <h2>17 de enero, 21:30 hs</h2>
+        <h3>Especial cumpleaños 29</h3>
       </div>
       <Diagonal />
     </div>
