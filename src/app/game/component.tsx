c030c7eae7a9df8f70373e711164b54315784c31
Iván Canga <ivancanga@outlook.com>
2025-01-04 01:05:24 -0300

    Numeradora game estilos

--- src/app/game/component.tsx
+++ src/app/game/component.tsx
@@ -4,6 +4,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { db } from "../../utils/firebase";
 import { doc, updateDoc, increment } from "firebase/firestore";
+import { PasswordContaianer } from "../styled";
 
 const secretCode = "123456"; // Código secreto que los equipos deben adivinar
 
@@ -22,14 +23,14 @@ export default function Game() {
       alert("Debes ingresar un código de 6 dígitos.");
       return;
     }
-  
+
     const docRef = doc(db, "games", "current");
-  
+
     if (code === secretCode) {
       try {
         // Actualiza el campo `winner` en Firestore con el equipo ganador
         await updateDoc(docRef, { winner: team });
-  
+
         // Redirige a la página de celebración
         router.push("/celebration");
       } catch (error) {
@@ -49,40 +50,56 @@ export default function Game() {
       }
     }
     setCode("");
-  };  
+  };
+
+  type TeamType = "rojo" | "verde" | "azul" | "amarillo";
+
+  const teamColors: Record<TeamType, string> = {
+    rojo: "bg-red-500",
+    verde: "bg-green-500",
+    azul: "bg-blue-500",
+    amarillo: "bg-yellow-500",
+  };
+
+  const teamColorClass =
+    team && teamColors[team.toLowerCase() as TeamType]
+      ? teamColors[team.toLowerCase() as TeamType]
+      : "bg-gray-500";
 
   return (
     <div className="flex flex-col items-center justify-center h-screen ">
-      <h1 className="text-xl font-bold mb-4">Equipo: {team}</h1>
-      <div className="mb-4 text-2xl">{code || "******"}</div>
+      <h1
+        className={`text-3xl font-bold mb-8 p-4 text-white rounded ${teamColorClass} uppercase w-80 text-center`}
+      >
+        Equipo {team}
+      </h1>
+      <PasswordContaianer className="mb-4">
+        {code || "******"}
+      </PasswordContaianer>
       <div className="grid grid-cols-3 gap-2 mb-4">
         {Array.from({ length: 9 }, (_, i) => i + 1).map((num) => (
           <button
             key={num}
-            className="p-4 bg-blue-500 text-white rounded-lg"
+            className="p-4 bg-white text-black text-8xl rounded-lg font-black digital-key"
             onClick={() => handleKeyPress(num.toString())}
           >
             {num}
           </button>
         ))}
         <button
-          className="p-4 bg-gray-500 text-white rounded-lg"
+          className="p-4 bg-red-500 text-black rounded-lg digital-key"
           onClick={() => setCode("")}
-        >
-          C
-        </button>
+        ></button>
         <button
-          className="p-4 bg-blue-500 text-white rounded-lg"
+          className="p-4 bg-white text-black text-8xl rounded-lg font-black digital-key"
           onClick={() => handleKeyPress("0")}
         >
           0
         </button>
         <button
-          className="p-4 bg-green-500 text-white rounded-lg"
+          className="p-4 bg-green-500 text-black rounded-lg digital-key"
           onClick={handleSubmit}
-        >
-          ✓
-        </button>
+        ></button>
       </div>
     </div>
   );
--- src/app/styled.ts
+++ src/app/styled.ts
@@ -113,3 +113,12 @@ export const ImageContainer = styled.img`
   transform: scale(1.3) rotate(-6deg);
   filter: blur(1.5px);
 `;
+
+export const PasswordContaianer = styled.div`
+  font-size: 96px;
+  border: 3px solid gray;
+  border-radius: 12px;
+  line-height: 75px;
+  height: 96px;
+  padding: 0 24px;
+`
