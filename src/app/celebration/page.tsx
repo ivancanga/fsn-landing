e3d36ce04c91aa579fb359836a094cb89fc1da93
Iván Canga <ivancanga@outlook.com>
2025-01-14 20:02:26 -0300

    Font size celebration confettis

--- src/app/celebration/page.tsx
+++ src/app/celebration/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useWindowSize } from "react-use"; // Para manejar el tamaño dinámico de la ventana
 import { db } from "../../utils/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import ReactConfetti from "react-confetti"; // Importa React-Confetti
 
 export default function Celebration() {
   const [winner, setWinner] = useState<string | null>(null);
+  const { width, height } = useWindowSize(); // Obtiene el tamaño de la ventana para el confeti
 
   useEffect(() => {
     const fetchWinner = async () => {
@@ -33,20 +36,30 @@ export default function Celebration() {
 
   return (
     <div
-      className={`flex flex-col items-center justify-center h-screen ${
+      className={`relative flex flex-col items-center justify-center h-screen w-full ${
         winner ? teamColors[winner] || "bg-gray-500" : "bg-gray-100"
       }`}
     >
+      {/* Confeti */}
+      {winner && <ReactConfetti width={width} height={height} recycle={true} />}
+
       {winner ? (
         <>
-          <h1 className="text-8xl font-bold text-white mb-4">
+          {/* Texto del título, adaptativo */}
+          <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-[180px] font-bold text-white text-center mb-4">
             ¡Felicidades al equipo {winner}! 🎉
           </h1>
-          <p className="text-4xl text-white mb-16">¡Gracias a todos por participar!</p>
+
+          {/* Texto del subtítulo, adaptativo */}
+          <p className="text-xl md:text-3xl lg:text-5xl xl:text-8xl text-white mb-8">
+            ¡Gracias a todos por participar!
+          </p>
+
+          {/* Imagen del diamante */}
           <img
             src="/diamond.gif"
             alt="Diamond"
-            className="w-80 h-80 animate-bounce"
+            className="w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80 animate-bounce"
           />
         </>
       ) : (
