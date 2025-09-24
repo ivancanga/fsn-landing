189b11f263a97c9aefbe29de299b40d8b8a87b31
Iván Canga <ivancanga@outlook.com>
2025-09-24 16:50:07 -0300

    fondo negro y parpadeante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,29 @@
 
 import { Logo } from "./styled";
 import Countdown from "./components/countdown";
+import styles from "./home-bloody.module.css";
 
 export default function Home() {
   return (
     <>
-      <div
-        className="flex flex-col items-center bg-black text-white overflow-hidden h-screen p-18"
-        style={{
-          backgroundImage: "url('/elritobg.png')",
-          backgroundSize: "80%", // <- contiene toda la imagen dentro del div
-          backgroundRepeat: "no-repeat", // <- evita la repetición
-          backgroundPosition: "center",
-          padding: "20px"
-        }}
-      >
-        <Logo>
-          <h1>FSN</h1>
-        </Logo>
-        <Countdown />
+      <div className={`${styles.wrapper} flex flex-col items-center text-white h-screen relative`}>
+        {/* Imagen central con parpadeo */}
+        <div className={styles.bgAnimated}>
+          <img src="/elritobg.png" alt="El rito de sangre" />
+        </div>
+
+        {/* Contenido en foreground */}
+        <div className="relative z-10 flex flex-col items-center">
+          <Logo>
+            <h1>FSN</h1>
+          </Logo>
+          <Countdown />
+        </div>
       </div>
-      <div className="text-white w-full text-center">
+
+      <div className="text-white w-full text-center mt-6">
         <h2>sábado 18 de octubre, 21:00 hs</h2>
-        <img src="/elrito.png" />
+        <img src="/elrito.png" alt="El rito logo" className="mx-auto" />
         <a href="https://www.instagram.com/fiestasinnombre/">@fiestasinnombre</a>
       </div>
     </>
